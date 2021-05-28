7ad15061711ac937c133fc44c52e3d1fd78505e0
huangrui <huangruiwang78@163.com>
2021-05-28 17:33:58 +0800

    init fontend project

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.home {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+</style>
