e90edc9741f6332178515587d1bf6f95e727e6d0
wangdan-cisdi <470058871@qq.com>
2021-07-20 14:02:02 +0800

    fix

--- frontend/src/views/storage/newStorage.vue
+++ frontend/src/views/storage/newStorage.vue
@@ -58,7 +58,7 @@ export default {
           trigger: 'blur',
         },
         {
-          pattern: /^[a-zA-Z0-9_\u4e00-\u9fa5.]+$/,
+          pattern: /^[^.][a-zA-Z0-9_\u4e00-\u9fa5.]+$/,
           message: () => {
             return t(`sourcePage.newUserErrorTip`);
           },
